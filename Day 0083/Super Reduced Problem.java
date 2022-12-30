d3cfefd2e4a70f2031455fccddfc7b4ac05d54da
Puja Choursiya <86359260+pujapkc@users.noreply.github.com>
2022-12-30 22:03:20 +0530

    Create Super Reduced Problem.java

---
+++ Day 0083/Super Reduced Problem.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+public class Solution {
+    public static void main(String [] args){
+       Scanner in = new Scanner(System.in);
+        reduceString(in.nextLine());    
+    }
+    static void reduceString(String s) {
+        boolean found = false;
+        for (int i = 0; i < s.length() - 1; i++) {
+            String a = s.substring(i, i + 1);
+            String b = s.substring(i + 1, i + 2);
+            if (a.equals(b)) {
+                s = s.substring(0, i) + s.substring(i + 2, s.length());
+
+                reduceString(s);
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            if(s.isEmpty()){
+                System.out.println("Empty String");
+            } else {
+                System.out.println(s);
+            }
+        }
+
+    }
+
+}
