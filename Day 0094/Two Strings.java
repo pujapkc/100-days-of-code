03432ec97ad19cdf2c8a9440738939b9ce17c96c
Puja Choursiya <86359260+pujapkc@users.noreply.github.com>
2023-01-14 21:16:45 +0530

    Create Two Strings.java

---
+++ Day 0094/Two Strings.java
@@ -0,0 +1,46 @@
+import java.io.*;
+import java.util.stream.*;
+
+class Result {
+
+    
+
+   public static String twoStrings(String s1, String s2) {
+        // Write your code here
+
+        String s1Result = s1.replaceAll("[" + s2 + "]", "");
+
+        if (s1Result.length() < s1.length()) {
+            return "YES";
+        }
+        return "NO";
+    }
+}
+
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int q = Integer.parseInt(bufferedReader.readLine().trim());
+
+        IntStream.range(0, q).forEach(qItr -> {
+            try {
+                String s1 = bufferedReader.readLine();
+
+                String s2 = bufferedReader.readLine();
+
+                String result = Result.twoStrings(s1, s2);
+
+                bufferedWriter.write(result);
+                bufferedWriter.newLine();
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
